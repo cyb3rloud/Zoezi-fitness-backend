d708d99d7cb1875c2de01733b976e0237a26c15c
ruthwekesa <wekesaruth569@gmail.com>
2023-01-10 00:21:10 +0300

    trainer model & migration

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,2 +1,6 @@
 class Trainer < ApplicationRecord
+    has_many :schedules
+    has_many :workouts, through: :schedules
+    has_many users:, through: :schedules
+    has_many :exercises, through: :schedules
 end
