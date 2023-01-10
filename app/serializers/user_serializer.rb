eaddabdcc4f4f00e0d592905ab07838e332b351f
ndungu1mac <macgeorgendungu@gmail.com>
2023-01-10 10:51:02 +0300

    can update cards

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
-class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :gender, :age, :contact, :email, :password
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :username, :weight, :name, :age, :height, :location, :profile_image
+  has_many :user_exercises
+  
 end
+
+#class TransactionSerializer < ActiveModel::Serializer
+  #attributes :id, :firstname, :lastname, :gender, :age, :contact, :email, :password
+#end
+#found the above code there, might be an update from rw-123 byte 
