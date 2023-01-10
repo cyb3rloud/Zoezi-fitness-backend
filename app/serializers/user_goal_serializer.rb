96757b335b0db48ec04f4ff159e84d90430f2a67
ndungu1mac <macgeorgendungu@gmail.com>
2023-01-10 10:44:17 +0300

    user_goals tables added

--- app/serializers/user_goal_serializer.rb
+++ app/serializers/user_goal_serializer.rb
@@ -1,3 +1,4 @@
-class MemberSerializer < ActiveModel::Serializer
-  attributes :id
+class UserGoalSerializer < ActiveModel::Serializer
+  attributes :id, :target_physique, :target_weight, :target_bmi, :tone_sculpt
+  has_one :user
 end
