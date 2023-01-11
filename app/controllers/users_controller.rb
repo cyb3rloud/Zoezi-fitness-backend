e1001f02be9d1dedfecec477516ff11ec55d78a3
Titus Mumo <titusmumo@gmail.com>
2023-01-11 15:52:02 +0300

    userController create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
     skip_before_action :authorize, only: [:create, :index]
     
-    def index
-        users = User.all
-        render json: users
-    end
+    # def index
+    #     users = User.all
+    #     render json: users
+    # end
     
     def create
         user = User.create!(user_params)
-        session[:user_id] = user.id
-        render json: user, status: :created
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else 
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end 
     end
     
     def show
