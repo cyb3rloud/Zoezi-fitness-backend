44355029f856ab76eedb83e176912498c79cde1e
ruthwekesa <wekesaruth569@gmail.com>
2023-01-11 12:32:11 +0300

    edit models attribues

--- db/migrate/20230109200038_create_sessions.rb
+++ db/migrate/20230109200038_create_sessions.rb
@@ -1,8 +1,7 @@
-class CreateSchedules < ActiveRecord::Migration[7.0]
+class CreateSessions < ActiveRecord::Migration[7.0]
   def change
-    create_table :schedules do |t|
-      t.integer :sched_id
-      t.string  :exercise_id
+    create_table :sessions do |t|
+      t.integer :session_id
       t.integer :trainer_id
       t.integer :user_id
       t.string :date
--- db/migrate/20230109210114_create_users.rb
+++ db/migrate/20230109210114_create_users.rb
@@ -2,13 +2,16 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.integer :user_id
-      t.string :firstname
-      t.string :lastname
-      t.integer :age
+      t.string  :firstname
+      t.string  :lastname
+      t.string  :email
+      t.string  :password
       t.integer :contact
-      t.string :gender
-      t.string :email
-      t.string :password
+      t.integer :age
+      t.string  :weight
+      t.string  :height
+      t.string  :target_height
+      t.string  :user_goal
       t.timestamps
     end
   end
