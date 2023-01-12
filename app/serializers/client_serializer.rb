1af7687c87e56f32c254110d27e4079858a01512
Titus Mumo <titusmumo@gmail.com>
2023-01-12 18:36:43 +0300

    edits

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,5 +1,5 @@
 class ClientSerializer < ActiveModel::Serializer
-  attributes :id, :email, :current_weight, :firstname,  :lastname, :age, :height, :contact,:password,:user_goal
+  attributes :id, :email, :current_weight, :firstname,  :lastname, :age, :height, :contact,:password,:client_goal
 
   has_many :exercises
 end
