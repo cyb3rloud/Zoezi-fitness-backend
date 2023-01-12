a69f140da76c3c2c3568310ce96195b05186d57f
Titus Mumo <titusmumo@gmail.com>
2023-01-12 20:33:34 +0300

    client controller edit

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,11 @@
 class Client < ApplicationRecord    
     has_many :workouts
+    has_many :trainers
     has_many :testimonials
-    has_many :trainers, through: :schedules
-    has_many :exercises, through: :workouts
+    has_many :schedules
+    has_many :exercises
+    # has_many :trainers, through: :schedules
+    # has_many :exercises, through: :workouts
 
     validates :firstname, presence: true
     validates :lastname, presence: true
