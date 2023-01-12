1e29036e16820b118809b1cf23dbd98f09746b43
Titus Mumo <titusmumo@gmail.com>
2023-01-12 09:41:37 +0300

    editting migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,74 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_094035) do
+  create_table "exercises", force: :cascade do |t|
+    t.integer "exercise_id"
+    t.string "exercise_name"
+    t.integer "trainer_id"
+    t.string "muscle_group"
+    t.string "activity"
+    t.integer "user_id"
+    t.integer "workout_id"
+    t.time "time_start"
+    t.time "time_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.integer "schedule_id"
+    t.integer "trainer_id"
+    t.integer "user_id"
+    t.string "date"
+    t.string "session"
+    t.time "session_start"
+    t.time "session_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.integer "testimonial_id"
+    t.string "testimony"
+    t.string "rating"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trainers", force: :cascade do |t|
+    t.integer "trainer_id"
+    t.string "firstname"
+    t.string "lastname"
+    t.integer "contact"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.string "password"
+    t.integer "contact"
+    t.integer "age"
+    t.string "height"
+    t.string "current_weight"
+    t.string "user_goal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.integer "workout_id"
+    t.string "workout_name"
+    t.integer "trainer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
