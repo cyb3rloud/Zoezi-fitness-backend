f6414305b6f4d5afc0f2048c413b119b53e1e687
Titus Mumo <titusmumo@gmail.com>
2023-01-12 23:19:10 +0300

    workout edits

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -1,3 +1,3 @@
 class ScheduleSerializer < ActiveModel::Serializer
-  attributes :id, :session_start, :date,  :session_end, :trainer_id, :user_id,:session
+  attributes :id, :session_start, :date,  :session_end, :trainer_id, :client_id,:session
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :clients, only: [:index, :create, :show, :update, :destroy]
   resources :trainers, only: [:index, :create, :show, :update, :destroy]
   resources :exercises, only: [:index, :create, :show, :update, :destroy]
-  resources :schedules, only: [:create, :show]
+  # resources :schedules, only: [:create, :show]
   resources :workouts, only: [:index, :create, :show]
   resources :schedules, only: [:index, :create, :show, :update, :destroy]
   resources :testimonials, only: [:index, :create, :show, :update, :destroy]
