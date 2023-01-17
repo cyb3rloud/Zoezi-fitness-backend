adf9ca7b3b8af673556d66f7d42b533808897823
Titus Mumo <titusmumo@gmail.com>
2023-01-17 23:45:39 +0300

    schedule relationship

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -1,3 +1,6 @@
 class ScheduleSerializer < ActiveModel::Serializer
   attributes :id, :session_start, :date,  :session_end, :trainer_id, :client_id,:session
+
+  has_many :client
+  belongs_to :trainer
 end
