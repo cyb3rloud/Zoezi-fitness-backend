509234def231ade0861709bdaf6152eb694e6942
Titus Mumo <titusmumo@gmail.com>
2023-01-17 21:23:47 +0300

    editing serailizers, models and seed file

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,11 +5,10 @@ class Client < ApplicationRecord
     has_many :schedules
     has_many :exercises
     # has_many :trainers, through: :schedules
-    has_many :exercises, through: :workouts
+    # has_many :exercises, through: :workouts
 
     validates :firstname, presence: true
     validates :lastname, presence: true
-    # validates :username, presence: true
     validates :age, numericality: { only_integer: true}
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { in: 6..20 }
