75b37f47f7e6b2501c4cd6a4341338f70d2a2abc
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2023-01-23 03:52:36 +0300

    seed new data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_195143) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_004604) do
   create_table "exercises", force: :cascade do |t|
     t.string "exercise_name"
     t.integer "trainer_id"
@@ -42,6 +42,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_195143) do
     t.integer "user_id"
     t.string "user_username"
     t.string "user_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trainer_dashboards", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.string "image_url"
+    t.integer "contact"
+    t.string "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
